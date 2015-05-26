a94338791dc4f807be0ea270f9c3f3de94159f06
rbong <roger.bongers@yahoo.com>
2015-05-26 14:52:56 -0400

    quick commit.

--- src/str.c
+++ src/str.c
@@ -2,10 +2,10 @@
 
 int str_cat (int _strs, char* _dest, char** _src)
 {
-    /* int tmp   = 0; */
-    int ret   = 0;
-    int i     = 0;
-    int bytes = 0;
+    /* int tmp = 0; */
+    int ret    = 0;
+    int i      = 0;
+    int _bytes = 0;
 
     if (_strs <= 0)
     {
@@ -16,16 +16,16 @@ int str_cat (int _strs, char* _dest, char** _src)
     if (_dest == NULL || _src == NULL)
     {
         ERR_PRINT (_EPTRNULL);
-        return -1;
+        ret = -1;
         return ret;
     }
 
     for (i = 0; i < _strs; i++)
     {
-        strcpy (_dest + bytes, _src [i]);
-        bytes += strlen (_src [i]);
+        strcpy (_dest + _bytes, _src [i]);
+        _bytes += strlen (_src [i]);
     }
-    _dest [bytes] = '\0';
+    _dest [_bytes] = '\0';
 
     /* if (tmp < 0) */
     /* { */
@@ -37,9 +37,9 @@ int str_cat (int _strs, char* _dest, char** _src)
 int str_cat_len (int _strs, char** _src)
 {
     /* int tmp = 0; */
-    int ret = 0;
-    int i;
-    char* s;
+    int   ret = 0;
+    int   i;
+    char* _s;
 
     if (_src == NULL)
     {
@@ -50,13 +50,13 @@ int str_cat_len (int _strs, char** _src)
 
     for (i = 0; i < _strs; i++)
     {
-        s = _src [i];
-        if (s == NULL)
+        _s = _src [i];
+        if (_s == NULL)
         {
             ERR_PRINT (_EPTRNULL);
             return -1;
         }
-        ret += strlen (s);
+        ret += strlen (_s);
     }
     return ret;
 }
