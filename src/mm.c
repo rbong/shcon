f891831d87d09a76b25228cd89e913f578968fbf
rbong <roger.bongers@yahoo.ca>
2015-05-25 22:44:56 -0400

    Sort of restored makefile.

--- src/mm.c
+++ src/mm.c
@@ -1,4 +1,65 @@
+// todo- add comments for function operation
 #include <stdio.h>
-int main ()
+#include <unistd.h>
+
+#include <err.h>
+#include <shm.h>
+
+int main (int argc, char** argv)
 {
+    int    ret = 0;
+    shm_t* shm = NULL;
+    char   buf [100];
+
+    ret = shm_t_from_path (&shm, NULL, "test");
+
+    if (ret < 0)
+    {
+        ERR_PRINT (0);
+        return 1;
+    }
+
+    if (argv [1] [0] == 'w')
+    {
+        do
+        {
+            if (fgets (buf, 100, stdin) == NULL)
+            {
+                ret = -1;
+                ERR_AT_LINE_SYS (0, errno);
+                err_set (_ESYSTEM);
+                break;
+            }
+
+            ret = shm_write (shm, buf, 100);
+
+            if (ret < 0)
+            {
+                break;
+            }
+        } while (buf != NULL && buf [0] != '\n');
+    }
+    else if (argv [1] [0] == 'r')
+    {
+        do
+        {
+            ret = shm_read (shm, buf, 100);
+            if (buf [0] != '\0')
+            {
+                printf ("%s", buf);
+                shm_write (shm, "\0", 1);
+            }
+            sleep (1);
+        } while (buf != NULL && buf [0] != '\n');
+    }
+
+    if (ret < 0)
+    {
+        ERR_PRINT (0);
+        return 1;
+    }
+
+    shm_t_del (&shm);
+
+    return 0;
 }
